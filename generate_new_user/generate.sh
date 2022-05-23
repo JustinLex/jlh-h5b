4c0c74651721ea7d1f97e2bccb6a4c130f16b296
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-05-23 20:16:06 +0200

    Add script for creating new users and add SA to tmeit namespace

---
+++ generate_new_user/generate.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# https://www.scottbrady91.com/openssl/creating-elliptical-curve-keys-using-openssl
+# https://stackoverflow.com/a/67336849
+
+echo enter username
+read -r username
+
+openssl ecparam -name prime256v1 -genkey -noout -out private-key.pem
+openssl ec -in private-key.pem -pubout -out public-key.pem
+openssl req -new -key private-key.pem -out "$username.csr" -subj "/CN=$username"
+
+cat <<EOF | kubectl apply -f -
+apiVersion: certificates.k8s.io/v1
+kind: CertificateSigningRequest
+metadata:
+  name: $username
+spec:
+  groups:
+  - system:authenticated
+  request: $(cat "$username.csr" | base64 | tr -d '\n')
+  signerName: kubernetes.io/kube-apiserver-client
+  usages:
+  - client auth
+EOF
+
+kubectl certificate approve $username
+kubectl get csr $username -o jsonpath='{.status.certificate}'  | base64 -d > $username.crt
+
+base64 -w 0 < $username.crt
