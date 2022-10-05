288c3fde07747e5188a4a585ffba569e13edb965
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-10-05 23:37:49 +0200

    Install nextcloud

--- applications/nextcloud/seal.sh
+++ applications/nextcloud/seal.sh
@@ -5,3 +5,11 @@ kubeseal -o yaml \
 --scope cluster-wide \
 < secret-kubegres-passwords-unsealed.yaml\
 > secret-kubegres-passwords.yaml
+
+# nextcloud-secrets
+kubeseal -o yaml \
+--controller-name sealed-secrets \
+--controller-namespace sealed-secrets \
+--scope cluster-wide \
+< secret-nextcloud-secrets-unsealed.yaml\
+> secret-nextcloud-secrets.yaml
--- applications/nextcloud/unseal.sh
+++ applications/nextcloud/unseal.sh
@@ -4,4 +4,8 @@ oc get secret -n sealed-secrets -l sealedsecrets.bitnami.com/sealed-secrets-key 
 kubeseal -o yaml --recovery-unseal --recovery-private-key /tmp/ss-cert < secret-kubegres-passwords.yaml \
 > secret-kubegres-passwords-unsealed.yaml
 
+# nextcloud-secrets
+kubeseal -o yaml --recovery-unseal --recovery-private-key /tmp/ss-cert < secret-nextcloud-secrets.yaml \
+> secret-nextcloud-secrets-unsealed.yaml
+
 rm /tmp/ss-cert
