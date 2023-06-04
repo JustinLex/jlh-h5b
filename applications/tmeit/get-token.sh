e69ad78359d29bce07320fab891f8b1f7b621e9f
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2023-06-04 03:24:23 +0200

    Delete old tmeit app

--- applications/tmeit/get-token.sh
+++
@@ -1,5 +0,0 @@
-echo base64\'d ca.crt
-kubectl -n tmeit get secret sa-token-gh-actions-deploy -o jsonpath='{.data.ca\.crt}'
-echo '
-plaintext token'
-kubectl -n tmeit get secret sa-token-gh-actions-deploy -o jsonpath='{.data.token}' | base64 -d
