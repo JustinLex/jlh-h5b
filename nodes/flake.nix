25e0ab5785d22e7d6ff1cfd11c66fed96e22054e
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2024-02-04 17:53:42 +0100

    nodes: feat: Pass self and unstablepkgs to modules

--- nodes/flake.nix
+++ nodes/flake.nix
@@ -1,74 +1,86 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
+    unstable.url =  "nixpkgs/nixos-unstable";
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
       # docs: https://github.com/nix-community/nixos-generators/blob/master/README.md
     };
   };
-  outputs = { self, nixpkgs, nixos-generators, ... }: {
-    packages.x86_64-linux = {
-      latios = nixos-generators.nixosGenerate {
+  outputs = { self, nixpkgs, unstable, nixos-generators, ... }: {
+    packages.x86_64-linux =
+      let
         system = "x86_64-linux";
-        format = "raw-efi";
-        modules = [
-           ./common.nix
-           ./nodes/latios.nix
-        ];
-        specialArgs = {
-            # additional arguments to pass to modules
-            nodeHostName = "latios";
+        unstablePkgs = unstable.legacyPackages.${system}; # https://github.com/nix-community/home-manager/issues/1538#issuecomment-1265293260
+      in
+      {
+        latios = nixos-generators.nixosGenerate {
+          system = "x86_64-linux";
+          format = "raw-efi";
+          modules = [
+             ./common.nix
+             ./nodes/latios.nix
+          ];
+          specialArgs = {
+              # additional arguments to pass to modules
+              self = self;
+              nodeHostName = "latios";
+          };
+        };
+        latias = nixos-generators.nixosGenerate {
+          system = "x86_64-linux";
+          format = "raw-efi";
+          modules = [
+             ./common.nix
+             ./nodes/latias.nix
+          ];
+          specialArgs = {
+              # additional arguments to pass to modules
+              self = self;
+              nodeHostName = "latias";
+          };
+        };
+        suicune = nixos-generators.nixosGenerate {
+          system = "x86_64-linux";
+          format = "raw-efi";
+          modules = [
+             ./common.nix
+             ./nodes/suicune.nix
+          ];
+          specialArgs = {
+              # additional arguments to pass to modules
+              self = self;
+              nodeHostName = "suicune";
+          };
+        };
+        chikorita = nixos-generators.nixosGenerate {
+          system = "x86_64-linux";
+          format = "raw-efi";
+          modules = [
+             ./common.nix
+             ./nodes/chikorita.nix
+          ];
+          specialArgs = {
+              # additional arguments to pass to modules
+              self = self;
+              nodeHostName = "chikorita";
+          };
+        };
+        porygontwo = nixos-generators.nixosGenerate {
+          system = "x86_64-linux";
+          format = "raw-efi";
+          modules = [
+             ./common.nix
+             ./nodes/porygontwo.nix
+          ];
+          specialArgs = {
+              # additional arguments to pass to modules
+              self = self;
+              nodeHostName = "porygontwo";
+              unstablePkgs = unstablePkgs;
+          };
         };
       };
-      latias = nixos-generators.nixosGenerate {
-        system = "x86_64-linux";
-        format = "raw-efi";
-        modules = [
-           ./common.nix
-           ./nodes/latias.nix
-        ];
-        specialArgs = {
-            # additional arguments to pass to modules
-            nodeHostName = "latias";
-        };
-      };
-      suicune = nixos-generators.nixosGenerate {
-        system = "x86_64-linux";
-        format = "raw-efi";
-        modules = [
-           ./common.nix
-           ./nodes/suicune.nix
-        ];
-        specialArgs = {
-            # additional arguments to pass to modules
-            nodeHostName = "suicune";
-        };
-      };
-      chikorita = nixos-generators.nixosGenerate {
-        system = "x86_64-linux";
-        format = "raw-efi";
-        modules = [
-           ./common.nix
-           ./nodes/chikorita.nix
-        ];
-        specialArgs = {
-            # additional arguments to pass to modules
-            nodeHostName = "chikorita";
-        };
-      };
-      porygontwo = nixos-generators.nixosGenerate {
-        system = "x86_64-linux";
-        format = "raw-efi";
-        modules = [
-           ./common.nix
-           ./nodes/porygontwo.nix
-        ];
-        specialArgs = {
-            # additional arguments to pass to modules
-            nodeHostName = "porygontwo";
-        };
-      };
-    };
   };
 }
