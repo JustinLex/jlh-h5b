95be56fcffe359eadd873aa110d96c2b12688107
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2024-02-10 13:24:50 +0100

    Refactor: Refactor node definitions in flake to iterate over the node list with a common node template

--- nodes/flake.nix
+++ nodes/flake.nix
@@ -5,82 +5,51 @@
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
-      # docs: https://github.com/nix-community/nixos-generators/blob/master/README.md
     };
   };
-  outputs = { self, nixpkgs, unstable, nixos-generators, ... }: {
-    packages.x86_64-linux =
-      let
-        system = "x86_64-linux";
-        unstablePkgs = unstable.legacyPackages.${system}; # https://github.com/nix-community/home-manager/issues/1538#issuecomment-1265293260
-      in
-      {
-        latios = nixos-generators.nixosGenerate {
-          system = "x86_64-linux";
-          format = "raw-efi";
-          modules = [
-             ./common.nix
-             ./nodes/latios.nix
-          ];
-          specialArgs = {
-              # additional arguments to pass to modules
-              self = self;
-              nodeHostName = "latios";
-          };
-        };
-        latias = nixos-generators.nixosGenerate {
-          system = "x86_64-linux";
-          format = "raw-efi";
-          modules = [
-             ./common.nix
-             ./nodes/latias.nix
-          ];
-          specialArgs = {
-              # additional arguments to pass to modules
-              self = self;
-              nodeHostName = "latias";
-          };
-        };
-        suicune = nixos-generators.nixosGenerate {
-          system = "x86_64-linux";
-          format = "raw-efi";
-          modules = [
-             ./common.nix
-             ./nodes/suicune.nix
-          ];
-          specialArgs = {
-              # additional arguments to pass to modules
-              self = self;
-              nodeHostName = "suicune";
-          };
-        };
-        chikorita = nixos-generators.nixosGenerate {
-          system = "x86_64-linux";
-          format = "raw-efi";
-          modules = [
-             ./common.nix
-             ./nodes/chikorita.nix
-          ];
-          specialArgs = {
-              # additional arguments to pass to modules
-              self = self;
-              nodeHostName = "chikorita";
-          };
-        };
-        porygontwo = nixos-generators.nixosGenerate {
-          system = "x86_64-linux";
-          format = "raw-efi";
-          modules = [
-             ./common.nix
-             ./nodes/porygontwo.nix
-          ];
-          specialArgs = {
-              # additional arguments to pass to modules
-              self = self;
-              nodeHostName = "porygontwo";
-              unstablePkgs = unstablePkgs;
-          };
-        };
-      };
+  outputs = { self, nixpkgs, unstable, nixos-generators, ... }:
+    let
+      unstablePkgs = unstable.legacyPackages.x86_64-linux; # https://github.com/nix-community/home-manager/issues/1538#issuecomment-1265293260
+      nodes = [
+        "latios"
+        "latias"
+        "suicune"
+        "chikorita"
+        "porygontwo"
+      ];
+      generate = (
+        # attrset Template for configuring NixOS generator for a given node
+        # docs: https://github.com/nix-community/nixos-generators/blob/master/README.md
+        nodename:
+          nixos-generators.nixosGenerate {
+            system = "x86_64-linux";
+            format = "raw-efi";
+            modules = [
+               ./common.nix
+               ./nodes/${nodename}.nix
+            ];
+            specialArgs = {
+                # additional arguments to pass to modules
+                self = self;
+                nodeHostName = nodename;
+                unstablePkgs = unstablePkgs;
+            };
+          }
+      );
+    in {
+
+      # This evaluates to: {"latios" = nixos-generators.nixosGenerate {...}; ... }
+      packages.x86_64-linux = builtins.listToAttrs (
+        map
+          ( nodename: { "name" = nodename; "value" = generate(nodename); } )
+          ( nodes )  # List of nodes to generate images for
+      );
+
+#        nixosConfigurations.porygontwo =
+#          let a = 1;
+#          in {
+#
+#          };
+
   };
 }
